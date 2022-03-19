654334d2d900735c31706973c9be73229eebdab7
Richard Sprague <rasprague@gmail.com>
2022-03-19 12:46:53 -0700

    add variable usage to m8c.sh

--- m8c.sh
+++ m8c.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+HWOUTDEVICE=0
+RATE=44100
 
 # audio routing
 export JACK_NO_AUDIO_RESERVATION=1
-jackd -d alsa -d hw:M8 -r44100 -p512 &
+jackd -d alsa -d hw:M8 -r$RATE -p512 &
 sleep 1
-alsa_out -j m8out -d hw:0 -r 44100 &
+alsa_out -j m8out -d hw:$HWOUTDEVICE -r $RATE &
 sleep 1
 jack_connect system:capture_1 m8out:playback_1
 jack_connect system:capture_2 m8out:playback_2
@@ -15,4 +17,4 @@ pushd /home/pi/code/m8c-piboy
 popd
 
 # clean up audio routing
-killall -s SIGINT jackd alsa_out
+killall -s SIGINT alsa_out jackd
