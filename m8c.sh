a1e340c59833d3ff3dabb633da3b03291e77d7e2
Richard Sprague <rasprague@gmail.com>
2022-03-19 16:52:19 -0700

    add audio input support via ENABLEINPUT variable in shell script

--- m8c.sh
+++ m8c.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 HWOUTDEVICE=0
+ENABLEINPUT=0
 RATE=44100
 
 # audio routing
 export JACK_NO_AUDIO_RESERVATION=1
 jackd -d alsa -d hw:M8 -r$RATE -p512 &
 sleep 1
+
+# setup output
 alsa_out -j m8out -d hw:$HWOUTDEVICE -r $RATE &
 sleep 1
 jack_connect system:capture_1 m8out:playback_1
 jack_connect system:capture_2 m8out:playback_2
+
+# setup input
+if [ $ENABLEINPUT -eq 1 ]; then
+  alsa_in -j m8in -d hw:$HWOUTDEVICE -r $RATE &
+  sleep 1
+  jack_connect m8in:capture_1 system:playback_1
+  jack_connect m8in:capture_2 system:playback_2
+fi
 
 # start m8 client
 pushd /home/pi/code/m8c-piboy
@@ -17,4 +28,4 @@ pushd /home/pi/code/m8c-piboy
 popd
 
 # clean up audio routing
-killall -s SIGINT alsa_out jackd
+killall -s SIGINT jackd alsa_out alsa_in
