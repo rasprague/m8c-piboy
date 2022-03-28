c3607eb32014278a48f33ac364105a91f93c9f8f
Richard Sprague <rasprague@gmail.com>
2022-03-27 22:15:34 -0700

    add rate cammand line argument to m8c.sh

--- m8c.sh
+++ m8c.sh
@@ -10,11 +10,12 @@ usage()
     echo "  options:"
     echo "    [ -i | --interface ] <I> - use audio interface I, defaults to 0"
     echo "      (see output from aplay -l command)"
+    echo "    [ -r | --rate ] <R> - use R sample rate, defaults to 44100"
     echo "    [ -e | --enable-input = enable audio input, defaults is off"
     echo "    [ -h | --help ] show this helpful message"
 }
 
-OPTIONS=$(getopt -o i:eh --long interface:,enable-input,help -- "$@")
+OPTIONS=$(getopt -o i:r:eh --long interface:,rate:,enable-input,help -- "$@")
 if [ $? -ne 0 ]; then
     usage
     exit 1
@@ -25,8 +26,10 @@ eval set -- "$OPTIONS"
 while true; do
     case "$1" in
 	-i|--interface)
-	    HWAUDIODEVICE=$2; shift 2 ;;
-	-e | --enable-input)
+	    HWAUDIODEVICE=$2 ; shift 2 ;;
+	-r|--rate)
+	    RATE=$2 ; shift 2 ;;
+	-e|--enable-input)
 	    ENABLEINPUT=1 ; shift ;;
 	-h|--help)
 	    usage ; shift ; exit 0 ;;
